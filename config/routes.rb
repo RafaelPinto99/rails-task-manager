5b1dc3ce407a70b6c98b7bcbd9f82eaf6d473bf8
Rafael Pinto <pintorafael1999@gmail.com>
2024-11-12 14:38:04 +0000

    Rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # Read all
+  get "tasks", to: "tasks#index", as: :tasks
+  # Create
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  # Read one tasks - The "show" route needs to be *after* "new" route
+  get "tasks/:id", to: "tasks#show", as: :task
+  # Update a task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  # Delete a task
+  delete "tasks/:id", to: "tasks#destroy"
 end
